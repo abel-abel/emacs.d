dd76adb2774d3beba78f6730dae327f6826659b1
liuxueyang <zypz457@sina.com>
2017-11-04 13:37:55 +0800

    add init.el file

---
+++ init.el
@@ -0,0 +1,62 @@
+;; Sun Oct 29 23:24:21 CST 2017
+
+(setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")))
+(package-initialize)
+(add-to-list 'default-frame-alist
+	     '(font . "Source Code Pro-15"))
+
+;; load ahungry theme
+(use-package ahungry-theme)
+(load-theme 'ahungry t)
+
+;; disable toolbar
+(tool-bar-mode -1)
+
+;; disable menubar
+(menu-bar-mode -1)
+
+;; Emacs frame transparency
+;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
+;;(set-frame-parameter (selected-frame) 'alpha <both>)
+(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+
+;; ==================================================
+;; Packages Installation
+;; ==================================================
+(setq use-package-always-ensure t)
+
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+(use-package
+  lispy
+  :config (add-hook
+	   'emacs-lisp-mode-hook
+	   (lambda () (lispy-mode 1))))
+
+(use-package swiper)
+(use-package counsel
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-selectable-prompt t)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history))
+
